d7ce7bfa3e2e9502d048191b343e5b69c3f48a9d
Tanvir Mahmud <tanvirmahmud@Tanvirs-MacBook-Air.local>
2023-03-29 17:31:54 +0600

    authentications system with django

---
+++ authentication/main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def landing(request):
+    return render(request, 'landing/landing.html')
+
+
+def sing_up(reqeust):
+    pass
+
+
+def log_in(request):
+    pass
+
+
+def home(reqeust):
+    pass
