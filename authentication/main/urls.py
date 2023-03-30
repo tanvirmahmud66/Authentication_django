0dba987bc3b2e2fb8331373579d493584a6a0786
Tanvir Mahmud <tanvirmahmud@Tanvirs-MacBook-Air.local>
2023-03-31 00:40:35 +0600

    authentications system with django

--- authentication/main/urls.py
+++ authentication/main/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.landing, name='landing'),
     path('signup/', views.sing_up, name='signup'),
     path('signin/', views.log_in, name='signin'),
+    path('profile/', views.profile, name='profile'),
+    path('logout/', views.logout_page, name='logout')
 ]
--- authentication/main/views.py
+++ authentication/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -28,7 +29,17 @@ def log_in(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return render(request, 'Home/profile.html')
+            return redirect('profile')
         else:
             return render(request, 'signin/signin.html')
     return render(request, 'signin/signin.html')
+
+
+@login_required(login_url='signin')
+def profile(request):
+    return render(request, 'Home/profile.html')
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('signin')
